08c55eae474094ad148dd2550d76ab7e01b7b509
YasinZhang <271155397@qq.com>
2019-03-05 21:37:50 +0800

    Add Power Alg

--- src/powerNum/PowerNumAlg.java
+++ src/powerNum/PowerNumAlg.java
@@ -1,29 +1,21 @@
 package powerNum;
 
 public class PowerNumAlg {
-    
-    public Integer power(int x, int n) {
-        return power(new Integer(x), new Integer(n));
-    }
 
-    public Integer power(Integer x, Integer n) {
+    public Integer power(int x, int n) {
+        if (n <= 1) {
+            return x;
+        }
+
         if (isEven(n)) {
-            return powerEven(x, n);
+            return power(x, n/2) * power(x, n/2);
         } else {
-            return powerEven(x, n-1) * x;
+            return power(x, (n-1)/2) * power(x, (n-1)/2) * x;
         }
     }
     
     private boolean isEven(Integer n) {
         return (n%2 == 0);
-    }
-    
-    private Integer powerEven(Integer x, Integer n) {
-        if (n <= 1) {
-            return x;
-        }
-        
-        return powerEven(x, n) * powerEven(x, n);
     }
     
 }
